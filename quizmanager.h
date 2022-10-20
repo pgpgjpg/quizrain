aa5cc0796a056a49697fd2a0a677c72101b1f747
sungwoo <voldmott@gmail.com>
2022-10-20 13:03:49 +0900

    main 조금 수정

--- quizmanager.h
+++ quizmanager.h
@@ -19,8 +19,8 @@ public:
 	void playQuiz();
     void fileRead(ifstream &fin);
     void showQuizList() const;
-    int getNumQuiz();
-    string getQuiz(int n);
+    int getNumQuiz(); // 문제의 개수 리턴
+    string getQuiz(int n);  //문제 리턴
     string callCheckAnswer(int n, string choice_answer); //n번 문제의 choice_answer 정답 입력 받아서 정답/오답 메시지 리턴
     vector<string> sendGetAnswers(); //보기 map에 띄워줌
 };
